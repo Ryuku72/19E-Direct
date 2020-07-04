a0b5a95c3bc432c56c69afd79b0b66379ac79036
Joshua K Bader <jaybshinsen@hotmail.com>
2020-07-05 02:25:04 +0800

    remove button works

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React, {useState} from "react";
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Departments from "./pages/Departments";
 import Roles from "./pages/Roles";
 import Employees from "./pages/Employees";
 import Navbar from "./components/Navbar/Navbar"
-import Card from "./components/Card/Card"
 import Footer from "./components/Footer/Footer"
 import Wrapper from "./components/Wrapper/Wrapper";
-import data from "./company.json"
+
 
 function App() {
-  const [developerState] = useState(data);
   return (
     <Router>
       <div>
@@ -22,17 +20,6 @@ function App() {
           <Route exact path="/departments" component={Departments} />
           <Route exact path="/roles" component={Roles} />
           <Route exact path="/employees" component={Employees} />
-          {developerState.map(result => (
-            <Card
-            key={result.name}
-            name={result.name}
-            department={result.department}
-            role={result.role}
-            manager={result.manager}
-            salary={result.salary}
-            email={result.email}
-          />
-          ))}
         </Wrapper>
         <Footer />
       </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
+import Card from "../components/Card/Card"
+import Data from "../company.json"
 
 function Home() {
+  const [search, setSearch] = useState(Data);
+  function removeEmployee(key) {
+    console.log(key)
+    const employees = search.filter(result => result.name !== key);
+    setSearch(employees);
+  };
     return (
         <div>
-       <h1>Home Screen</h1>
+          {search.map(result => (
+            <Card
+            removeEmployee={removeEmployee}
+            key={result.name}
+            name={result.name}
+            department={result.department}
+            role={result.role}
+            manager={result.manager}
+            salary={result.salary}
+            email={result.email}
+          />
+          ))}
       </div>
     )
 }
