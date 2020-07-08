f1b02e99142beb4012bf4aca422139238e973b6c
Joshua K Bader <jaybshinsen@hotmail.com>
2020-07-08 13:19:48 +0800

    fixed router issues

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -13,10 +13,10 @@ function Navbar() {
       </div>
       <div className="h-8 bg-gray-900 flex border-gray-600 border-b-2 text-white">
       <div className="text-left w-1/2">
-        <a className="">
+        <span className="">
           <Link to="/" className={location.pathname === "/" || window.location.pathname === "/search" ? "text-red-500" : "text-white"}>
-            <span className="ml-7 hover:text-orange-400 block ml-8 hover:underline">
-        <i className="fa fa-search mx-2" aria-hidden="true"></i>Search</span></Link></a>
+            <p className="ml-7 hover:text-orange-400 block ml-8 hover:underline">
+        <i className="fa fa-search mx-2" aria-hidden="true"></i>Search</p></Link></span>
       </div>
       <div className="block w-1/2 text-right mr-4">
         <Link
